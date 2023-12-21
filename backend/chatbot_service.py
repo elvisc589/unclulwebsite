170847e9ce9b0ff170d7b6b394c8dc9b2447edd1
elvischiqui <elvisrchiqui@gmail.com>
2023-12-20 22:13:02 -0500

    Remove unecessary imports

--- backend/chatbot_service.py
+++ backend/chatbot_service.py
@@ -1,14 +1,8 @@
-from fastapi import Depends, HTTPException
 from env import getenv
-import pandas as pd
-import matplotlib.pyplot as plt
-from transformers import GPT2TokenizerFast
 from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 
