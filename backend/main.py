0fcb97b90b1634f42752338b31188e96fef18130
elvischiqui <elvisrchiqui@gmail.com>
2023-12-20 23:49:55 -0500

    Add route to fix 404 error

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,10 @@ class ChatMessage(BaseModel):
     message: str
 
 app = FastAPI()
-origins = ["http://localhost:"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
