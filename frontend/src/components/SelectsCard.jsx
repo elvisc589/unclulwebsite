10d4e10bec7b74bc2cb406439d5ec07bfc6b18fe
David Villavicencio <111402717+dsvill06@users.noreply.github.com>
2024-07-14 19:48:16 -0400

    Updated Lines

--- frontend/src/components/SelectsCard.jsx
+++ frontend/src/components/SelectsCard.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 const SelectsCard = (props) => {
     return(
-<div>
+<div className='container '>
 <div className='relative pb-44 mb-9'>
          
             <img className='top-0 pt-6' src={props.bg} loading="lazy" alt='/'/>
         
 <div className='bg-gray-900/5 absolute w-full h-56'>
     <div className='space-y-9'>
-            <h2 className='text-border-black py-4 text-2xl font-bold text-white absolute'>
+            <h2 className='text-border-black py-4 text-2xl font-bold text-white absolute '>
                     {props.h2}
                 </h2>
            
-                <p className='text-border-black py-4 text-l text-white absolute'>           
+                <p className='text-border-black py-4 pt-10 text-l text-white absolute'>           
                     {props.p1}
                    <div>
                     {props.p2}
